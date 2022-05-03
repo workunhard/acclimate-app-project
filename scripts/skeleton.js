25ce320bc0a621f8e28237d4dca38294f5bbdf4a
Jason Lee <jlee1473@my.bcit.ca>
2022-05-03 16:08:08 -0700

    Added nav bar and hamburger menu -Jason

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,3 +3,12 @@ function loadSkeleton(){
     console.log($('#footerPlaceholder').load('../text/footer.html'));
 }
 loadSkeleton();  
+
+function expand() {
+    var x = document.getElementById("navLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
