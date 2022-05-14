d044e07f5d402e955b8efcf3ffc5786f79faaee6
Code Workun <cworkun@my.bcit.ca>
2022-05-13 23:52:00 -0700

    More S3 stuff

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -10,13 +10,18 @@
   })();
   
   function getSignedRequest(file){
+      console.log("get signed request");
     const xhr = new XMLHttpRequest();
     xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
     xhr.onreadystatechange = () => {
+        console.log("onreadystatechange");
       if(xhr.readyState === 4){
+        console.log("readystate == 4");
         if(xhr.status === 200){
+            console.log("readystate == 200");
           const response = JSON.parse(xhr.responseText);
           uploadFile(file, response.signedRequest, response.url);
+          console.log("fileuploaded");
         }
         else{
           alert('Could not get signed URL.');
