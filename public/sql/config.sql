42fe473a0324d4d78d0b7e8ffae5f0fc019b4a43
Code Workun <cworkun@my.bcit.ca>
2022-05-13 22:40:08 -0700

    Prepping for S3 image uploads

--- public/sql/config.sql
+++ public/sql/config.sql
@@ -10,10 +10,4 @@ CREATE TABLE bby23_user (
 
 INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("Code", "test@acclimate.com", "abcdefg", true, NULL);
 INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("Bruce", "bruce_link@bcit.ca", "abc123", false, NULL);
-INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("John", "john_romero@bcit.ca", "abc123", false, NULL);
-
-
-
-
-
-            
+INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("John", "john_romero@bcit.ca", "abc123", false, NULL);          
