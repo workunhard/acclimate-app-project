4212f6c29856eebee22e06e1a5150547cc8ce8db
Code Workun <cworkun@my.bcit.ca>
2022-05-13 17:37:00 -0700

    Troubleshooting

--- public/sql/config.sql
+++ public/sql/config.sql
@@ -1,12 +1,19 @@
 CREATE TABLE bby23_user (
     ID int NOT NULL AUTO_INCREMENT,
     name varchar(30),
-    email varchar(30),
+    email varchar(30) UNIQUE,
     password varchar(30),
     admin boolean,
+    avatar varchar (500) UNIQUE,
     PRIMARY KEY (ID)
     );
+
+INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("Code", "code@acclimate.com", "abcdefg", true, NULL);
+INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("Bruce", "bruce_link@bcit.ca", "abc123", false, NULL);
+INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("John", "john_romero@bcit.ca", "abc123", false, NULL);
+
+
+
+
+
             
-INSERT INTO bby23_user (name, email, password, admin) VALUES ("$User", "test@acclimate.com", "abcdefg", true);
-INSERT INTO bby23_user (name, email, password, admin) VALUES ("Bruce", "bruce_link@bcit.ca", "abc123", false);
-INSERT INTO bby23_user (name, email, password, admin) VALUES ("John", "john_romero@bcit.ca", "abc123", false);
