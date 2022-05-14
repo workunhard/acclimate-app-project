5115cdc922d7fac12daeac6614e2afa42ef49777
Jason Lee <jlee1473@my.bcit.ca>
2022-05-13 19:13:16 -0700

    Changed admin email to test@acclimate.com -Jason

--- public/sql/config.sql
+++ public/sql/config.sql
@@ -8,7 +8,7 @@ CREATE TABLE bby23_user (
     PRIMARY KEY (ID)
     );
 
-INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("Code", "code@acclimate.com", "abcdefg", true, NULL);
+INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("Code", "test@acclimate.com", "abcdefg", true, NULL);
 INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("Bruce", "bruce_link@bcit.ca", "abc123", false, NULL);
 INSERT INTO bby23_user (name, email, password, admin, avatar) VALUES ("John", "john_romero@bcit.ca", "abc123", false, NULL);
 
