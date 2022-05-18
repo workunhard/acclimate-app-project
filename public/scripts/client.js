6a303f740635b8aa61cd3a759e069f123f4520ed
Waleed <waleedurrehman12@gmail.com>
2022-05-18 16:57:34 -0700

    Sign up working - still rough

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,98 +1,97 @@
 ready(function () {
 
-    function ajaxGET(url, callback) {
+	function ajaxGET(url, callback) {
 
-        const xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-                //console.log('responseText:' + xhr.responseText);
-                callback(this.responseText);
+		const xhr = new XMLHttpRequest();
+		xhr.onload = function () {
+			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+				//console.log('responseText:' + xhr.responseText);
+				callback(this.responseText);
 
-            } else {
-                console.log(this.status);
-            }
-        }
-        xhr.open("GET", url);
-        xhr.send();
-    }
+			} else {
+				console.log(this.status);
+			}
+		}
+		xhr.open("GET", url);
+		xhr.send();
+	}
 
-    function ajaxPOST(url, callback, data) {
+	function ajaxPOST(url, callback, data) {
 
-        let params = typeof data == 'string' ? data : Object.keys(data).map(
-            function (k) { return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
-        ).join('&');
-        console.log("params in ajaxPOST", params);
+		let params = typeof data == 'string' ? data : Object.keys(data).map(
+			function (k) { return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+		).join('&');
+		console.log("params in ajaxPOST", params);
 
-        const xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-                callback(this.responseText);
+		const xhr = new XMLHttpRequest();
+		xhr.onload = function () {
+			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+				callback(this.responseText);
+			} else {
+				console.log(this.status);
+			}
+		}
+		xhr.open("POST", url);
+		xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.send(params);
+	}
 
-            } else {
-                console.log(this.status);
-            }
-        }
-        xhr.open("POST", url);
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.send(params);
-    }
+	document.querySelector("#submit").addEventListener("click", function (e) {
+		e.preventDefault();
+		let email = document.getElementById("email");
+		let password = document.getElementById("password");
+		let queryString = "email=" + email.value + "&password=" + password.value;
 
-    document.querySelector("#submit").addEventListener("click", function (e) {
-        e.preventDefault();
-        let email = document.getElementById("email");
-        let password = document.getElementById("password");
-        let queryString = "email=" + email.value + "&password=" + password.value;
+		ajaxPOST("/login", function (data) {
 
-        ajaxPOST("/login", function (data) {
+			if (data) {
+				let dataParsed = JSON.parse(data);
+				console.log(dataParsed);
+				if (dataParsed.status == "fail") {
+					document.getElementById("serverMsg").innerHTML = dataParsed.msg;
+				} else {
+					window.location.replace("/dashboard");
+				}
+			}
+		}, queryString);
+	});
 
-            if (data) {
-                let dataParsed = JSON.parse(data);
-                console.log(dataParsed);
-                if (dataParsed.status == "fail") {
-                    document.getElementById("serverMsg").innerHTML = dataParsed.msg;
-                } else {
-                    window.location.replace("/dashboard");
-                }
-            }
-        }, queryString);
-    });
+	document.querySelector("#signUp").addEventListener("click", function (e) {
+		e.preventDefault();
+		$('#inputBox').load('/text/signup.html');
+		const button = document.getElementById('submit');
+		button.value = "Sign-Up";
+		button.id = "signupsubmission";
 
-    document.querySelector("#signUp").addEventListener("click", function(e) {
-        e.preventDefault();
-        $('#inputBox').load('/text/signup.html');
-        const button = document.getElementById('submit');
-        button.value = "Sign-Up";
-        button.id = "signupsubmission";
-    });
+		document.querySelector("#signupsubmission").addEventListener("click", function (e) {
+			e.preventDefault();
+			let name = document.getElementById("name");
+			let email = document.getElementById("email");
+			let password = document.getElementById("password");
+			let queryString = "name=" + name.value + "&email=" + email.value + "&password=" + password.value + "&admin=" + 0;
 
-    document.querySelector("#signupsubmission").addEventListener("click", function (e) {
-        e.preventDefault();
-        let name = document.getElementById("name");
-        let email = document.getElementById("email");
-        let password = document.getElementById("password");
-        let queryString = "name=" + name.value + "&email=" + email.value + "&password=" + password.value;
+			ajaxPOST("/add-user", function (data) {
 
-        ajaxPOST("/add_user", function (data) {
+				if (data) {
+					let dataParsed = JSON.parse(data);
+					console.log(dataParsed);
+					if (dataParsed.status == "fail") {
+						document.getElementById("errorMsg").innerHTML = dataParsed.msg;
+					} else {
+						window.location.replace("/dashboard");
+					}
+				}
+			}, queryString);
+		});
+	});
 
-            if (data) {
-                let dataParsed = JSON.parse(data);
-                console.log(dataParsed);
-                if (dataParsed.status == "fail") {
-                    document.getElementById("errorMsg").innerHTML = dataParsed.msg;
-                } else {
-                    window.location.replace("/dashboard");
-                }
-            }
-        }, queryString);
-    });
-    
 });
 
 function ready(callback) {
-    if (document.readyState != "loading") {
-        callback();
-    } else {
-        document.addEventListener("DOMContentLoaded", callback);
-    }
+	if (document.readyState != "loading") {
+		callback();
+	} else {
+		document.addEventListener("DOMContentLoaded", callback);
+	}
 }
