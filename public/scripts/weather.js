996901acb9163f68b2cf144466ab1c66c902b995
Waleed <waleedurrehman12@gmail.com>
2022-05-19 09:47:03 -0700

    Added Weather.js to get the weather for the user location

---
+++ public/scripts/weather.js
@@ -0,0 +1,32 @@
+
+
+
+function getWeather() {
+  const lat = posOutside.lat;
+  const long = posOutside.lng;
+
+  const key = '01a2ba83bf76f2ed62c6a9cc680e3c6f';
+  const lang = 'en';
+  const units = 'metrics';
+  let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${key}&units=${units}&lang=${lang}`;
+
+
+  fetch(url)
+    .then(resp=> {
+      if (!resp.ok) {
+        console.log(resp.statusText);
+      } else {
+        return resp.json();
+      }
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(console.err)
+}
+
+function showWeather() {
+  
+}
+
+// weather();
