9e028b71ba94a56ba1a60ab7fe4703bee4b6a2ed
Code Workun <cworkun@my.bcit.ca>
2022-05-27 15:56:31 -0700

    Added comments

--- public/scripts/rain.js
+++ public/scripts/rain.js
@@ -1,9 +1,14 @@
+// Easter Egg Script: Making it Rain (CSS)
+
+// Set the number of raindrops to generate
 var numDrops = 700;
 
+// Helper function for createRain() to establish animation bounds
 function randRange(minNum, maxNum) {
     return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
 }
 
+// Function to create CSS rain effect
 function createRain() {
     for (i = 1; i < numDrops; i++) {
         var dropLeft = randRange(0, 1600);
@@ -15,6 +20,7 @@ function createRain() {
 }
 createRain();
 
+// Function to toggle rain on/off
 function toggleRain() {
     var rain = document.getElementById("rain");
     if (!rain.style.display || rain.style.display == "none") {
